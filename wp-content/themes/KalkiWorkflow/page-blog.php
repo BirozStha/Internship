7ca891ba9d0353456d60ba730ad59025fa78b9d4
Biroj Shrestha <birozshrestha@gmail.com>
2025-02-13 10:47:54 +0545

    small update

--- wp-content/themes/KalkiWorkflow/page-blog.php
+++ wp-content/themes/KalkiWorkflow/page-blog.php
@@ -6,27 +6,29 @@ get_header();
 ?>
 
 <?php
-    $category = get_category_by_slug('blog'); // Fetch category by slug
-    if ($category) :
+$category = get_category_by_slug('blog'); // Fetch category by slug
+if ($category) :
 ?>
     <h2 class="blog-title"><?php echo esc_html($category->name); ?></h2>
-    <?php endif; ?>
+<?php endif; ?>
 
 <div class="blog-container">
 
     <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
     $args = array(
         'post_type'      => 'post',
-        'posts_per_page' => -1,
+        'posts_per_page' => 8, 
+        'paged'          => $paged, 
         'post_status'    => 'publish',
-        'paged'          => $paged,
         'orderby'        => 'date',
         'order'          => 'ASC',
         'tax_query' => array(
             array(
                 'taxonomy' => 'category',
                 'field'    => 'slug',
-                'terms'    => 'Blog',
+                'terms'    => 'blog',
             ),
         ),
     );
@@ -49,19 +51,23 @@ get_header();
             </div>
     <?php
         endwhile;
-         // Pagination
-         echo '<div class="pagination">';
-         echo paginate_links(array(
-             'total' => $query->max_num_pages,
-             'current' => max(1, get_query_var('paged')),
-             'prev_text' => '« Prev',
-             'next_text' => 'Next »',
-         ));
-         echo '</div>';
-        wp_reset_postdata();
-    else :
-        echo '<p>No posts found.</p>';
-    endif;
     ?>
-</div>
-<?php get_footer();?>
+        
+    </div>
+    <?php
+    echo '<div class="pagination">';
+    echo paginate_links(array(
+        'total' => $query->max_num_pages,
+        'current' => max(1, get_query_var('paged')),
+        'prev_text' => '« Prev',
+        'next_text' => 'Next »',
+    ));
+    echo '</div>';
+
+    wp_reset_postdata();
+else :
+    echo '<p>No posts found.</p>';
+endif;
+?>
+
+<?php get_footer(); ?>
