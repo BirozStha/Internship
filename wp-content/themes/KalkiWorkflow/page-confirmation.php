ea9a4c4db642c093583837816f5e5facd6469137
Biroj Shrestha <birozshrestha@gmail.com>
2025-02-25 11:13:01 +0545

    appointment css finished

--- wp-content/themes/KalkiWorkflow/page-confirmation.php
+++ wp-content/themes/KalkiWorkflow/page-confirmation.php
@@ -5,6 +5,9 @@ get_header();
 <section class="confirmation-container">
     <h2 class="confirmation-title">Booking Confirmed!</h2>
     <p class="confirmation-message">Thank you! Your appointment has been scheduled. We'll contact you soon.</p>
-    <a class="home-link" href="<?php echo site_url(); ?>">Back to Home</a>
+    <div class="a-btn">
+        <a class="home-link" href="<?php echo site_url(); ?>">Back to Home</a>
+        <a class="home-link" href="<?php echo site_url('/booking'); ?>">Back to Booking</a>
+    </div>
 </section>
 <?php get_footer(); ?>  
